cd3aeae7cb53d9d4b64e0a4439340398c3d77384
Ozgur Ozhan <ozgur2096@gmail.com>
2023-11-16 13:52:42 +0100

    google api key needed

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,46 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-console.log(process.env);
+import axios from 'axios';
 
 const form = document.querySelector('form')!;
-// const addressInput = document.getElementById('address')! as HTMLInputElement;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'api key';
+
+// declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
-  // const enteredAddress = addressInput.value;
+  const enteredAddress = addressInput.value;
 
-  // send address to Google API
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById('map') as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        }
+      );
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch(err => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-form.addEventListener('submit', e => {
-  searchAddressHandler(e);
-});
-
-// console.log(process.env.GOOGLE_API_KEY);
+form.addEventListener('submit', searchAddressHandler);
